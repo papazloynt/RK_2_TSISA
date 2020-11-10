fdf98aba65412282e3e789c7e18be45c62d6b108
papazloynt <41233824+papazloynt@users.noreply.github.com>
2020-11-10 13:30:42 +0300

    Add files via upload
    Done

---
+++ RK_2_TSISA.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+#include <algorithm>
+
+//14 variant
+// 25% ; 6 points
+
+
+double Function(const double x, const double y)
+{
+	return (-log(1 + x * x + y * y) + x * x / 2);
+}
+
+
+bool max(std::pair<double, double> lhs, std::pair<double, double> rhs)
+{ return Function(lhs.first, lhs.second) > Function(rhs.first, rhs.second); }
+
+void mutation(std::pair<double, double>& p) {
+	if (rand() % 100 < 25) {
+		double tmp = p.first;
+		p.first = p.second;
+		p.second = tmp;
+	}
+}
+
+void crossover(const std::pair<double, double>& Ihs, std::pair<double, double>& rhs, std::vector<std::pair<double, double>>& vec) {
+	std::pair<double, double> buf1, buf2;
+	buf1.first = lhs.first;
+	buf1.second = rhs.second;
+	buf2.first = rhs.first;
+	buf2.second = lhs.second;
+	mutation(buf1);
+	mutation(buf2);
+	vec.push_back(buf1);
+	vec.push_back(buf2);
+}
+
+
+void R_I_P(std::vector<std::pair<double, double>>& vec) {
+	std::sort(vec.begin(), vec.end(), max);
+	vec.erase(vec.begin() + 4, vec.end());
+}
+
+
+void print(std::vector<std::pair<double, double>>& vec) {
+
+	double sum = 0;
+	for (size_t i = 0; i < 4; i++) {
+		std::cout << "X = " << vec[i].first << "   Y = " << vec[i].second
+			<< "   Fit  function = " << Function(vec[i].first, vec[i].second) << std::endl;
+		sum += Function(vec[i].first, vec[i].second);
+	}
+	double mid = sum / 4;
+	std::cout << "Middle Fit = " << mid
+		<< "   Max Fit = " << Function(vec[0].first, vec[0].second) << std::endl
+		<< std::endl;
+}
+
+
+int main() {
+	std::vector<std::pair<double, double>> vec(6);
+	std::random_device r;
+
+	for (size_t i = 0; i < 6; i++) {
+		vec[i].first = 1/ r();
+		vec[i].second = 1/ r();
+	}
+
+	std::sort(vec.begin(),vec.end(), max);
+
+	for (size_t i = 0; i < 100; i++) {
+		print(vec);
+		crossover(vec[0], vec[1], vec);
+		crossover(vec[0], vec[2], vec);
+		crossover(vec[0], vec[3], vec);
+		crossover(vec[0], vec[4], vec);
+		crossover(vec[0], vec[5], vec);
+		R_I_P(vec);
+	}
+
+
+}
